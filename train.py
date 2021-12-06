361ef091dd69b2d9f1f63fd5b801ff0cdc4f9055
Craig Smith <nctiggy@gmail.com>
2021-12-06 11:19:51 -0800

    prediction

--- train.py
+++ train.py
@@ -7,55 +7,58 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 help_msg = "This loads in a trained modeal and returns a prediction"
 parser = argparse.ArgumentParser(description=help_msg)
 parser.add_argument("-e", "--epochs", type=int, help="Path to the model")
-parser.add_argument("-s", "--steps", type=int, help="Link to image")
 args = parser.parse_args()
 
 model = tf.keras.models.Sequential([
-    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
-    # This is the first convolution
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.Conv2D(16,
+                           (3, 3),
+                           activation='relu', input_shape=(300, 300, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
-    # The second convolution
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    # The third convolution
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    # The fourth convolution
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    # The fifth convolution
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    # Flatten the results to feed into a DNN
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Flatten(),
-    # 512 neuron hidden layer
     tf.keras.layers.Dense(512, activation='relu'),
-    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
 model.compile(loss='binary_crossentropy',
-              optimizer=RMSprop(lr=0.001),
+              optimizer=RMSprop(learning_rate=0.001),
               metrics=['acc'])
 
-# All images will be rescaled by 1./255
-train_datagen = ImageDataGenerator(rescale=1/255)
+train_datagen = ImageDataGenerator(rescale=1/255,
+                                   validation_split=0.2)
 
-# Flow training images in batches of 128 using train_datagen generator
 train_generator = train_datagen.flow_from_directory(
-        '/Users/craigsmith/Downloads/horse-or-human',  # This is the source directory for training images
-        target_size=(300, 300),  # All images will be resized to 150x150
-        batch_size=128,
-        # Since we use binary_crossentropy loss, we need binary labels
-        class_mode='binary')
+        '/Users/craigsmith/Downloads/horse-or-human',
+        target_size=(300, 300),
+        batch_size=64,
+        class_mode='binary',
+        subset='training')
 
-history = model.fit_generator(
+validation_generator = train_datagen.flow_from_directory(
+        '/Users/craigsmith/Downloads/horse-or-human',
+        target_size=(300, 300),
+        batch_size=96,
+        class_mode='binary',
+        subset='validation')
+
+history = model.fit(
       train_generator,
-      steps_per_epoch=args.steps,
+      steps_per_epoch=train_generator.samples // 96,
       epochs=args.epochs,
+      validation_data=validation_generator,
+      validation_steps=validation_generator.samples // 96,
       verbose=1)
 
+print(history.__dict__)
+print('cnvrg_tag_test_accuracy: ', history.history['val_acc'][-1])
+print('cnvrg_tag_test_loss: ', history.history['val_loss'][-1])
 if not os.path.exists('output'):
     os.mkdir('output')
 model.save('output/imagizer.model.h5')
